0384000cad0ca3c0aed70c6b3b70043c9d5b991b
RamyaSreeDesemsetty <ramyadesemsetty@gmail.com>
2024-11-13 22:01:10 +0530

    changes made

---
+++ src/main/java/com/kafka/scbkafkaexampleone/ScbkafkaexampleoneApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.scbkafkaexampleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbkafkaexampleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScbkafkaexampleoneApplication.class, args);
+	}
+
+}
